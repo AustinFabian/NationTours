c51d7afb8dd715704649bf6df8aaac0e756269e6
AustinFabian <run2austinfabian@gmail.com>
2022-07-18 02:14:53 +0100

    God's good

--- controllers/bookingsController.js
+++ controllers/bookingsController.js
@@ -46,7 +46,7 @@ exports.checkoutSession = catchAsync(async(req,res,next)=>{
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.display_items[0].amount / 100;
+    const price = session.amount_total / 100;
     await Bookings.create({ tour, user, price });
     console.log(Bookings);
   };
